2d417e2b19c728e785f857b5b628291991635d16
dqminh <dqminh2810@gmail.com>
2023-07-21 11:20:31 +0200

    add cron

--- consumer/index.js
+++ consumer/index.js
@@ -1,6 +1,14 @@
 const consume = require("./consumer.js")
+const cron = require("node-cron");
+const fs = require('fs');
+
 consume().catch((err) => {
 	console.error("error in consumer: ", err)
 })
-
-// add cron to delete files about 1 week
+// add cron to delete files every midnight
+cron.schedule("0 0 0 * * *", function () {
+	console.log("---------------------Start remove in day video recording---------------------");
+	dir = `./NAS/`
+	fs.rmSync(dir, { recursive: true, force: true });
+	console.log("---------------------End remove in day video recording---------------------");
+  });
